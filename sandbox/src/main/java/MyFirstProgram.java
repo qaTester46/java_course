419ebc5450690946dbc54a7f91973dc5b35ba4e0
Sergey Maltsev <yellsn@bk.ru>
2021-05-29 15:30:32 +0300

    Выполнение 1,2 и 3 пункта задания

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,13 @@
 public class MyFirstProgram {
 
-  public static void main(String[] args) {
-System.out.println("Hellow, world");
-  }
+    public static void main(String[] args) {
 
+        Points p = new Points(3, 4, 5, 6);
+        System.out.println("Расстояние между точками с координатами (" + p.x1 + ";" + "" + p.y1 + ") и" +
+                " (" + p.x2 + "" + ";" + p.y2 + ")" + " равно: " + distance(p));
+    }
+
+    public static double distance(Points points) {
+        return Math.sqrt((points.x1 - points.x2) * (points.x1 - points.x2) + (points.y1 - points.y2) * (points.y1 - points.y2));
+    }
 }
